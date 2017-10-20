bcc61d278b44c1bda91a8e70701a3208e451081d
Andrey Kurilov <andrey.kurilov@emc.com>
2017-10-20 16:38:11 +0300

    save

--- src/main/java/com/emc/mongoose/storage/driver/hdfs/HdfsStorageDriver.java
+++ src/main/java/com/emc/mongoose/storage/driver/hdfs/HdfsStorageDriver.java
@@ -19,6 +19,7 @@ import com.emc.mongoose.ui.config.storage.net.node.NodeConfig;
 import com.emc.mongoose.ui.log.LogUtil;
 import com.emc.mongoose.ui.log.Loggers;
 
+import com.github.akurilov.commons.io.IoUtil;
 import com.github.akurilov.commons.system.SizeInBytes;
 
 import org.apache.hadoop.conf.Configuration;
@@ -38,6 +39,8 @@ import org.apache.logging.log4j.Level;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.nio.charset.StandardCharsets;
 import java.rmi.RemoteException;
 import java.rmi.ServerException;
@@ -46,6 +49,10 @@ import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.emc.mongoose.api.model.io.task.IoTask.Status.ACTIVE;
+import static com.emc.mongoose.api.model.io.task.IoTask.Status.FAIL_IO;
+import static com.github.akurilov.commons.io.IoUtil.REUSABLE_BUFF_SIZE_MAX;
 
 public class HdfsStorageDriver<I extends Item, O extends IoTask<I>>
 extends NioStorageDriverBase<I, O> {
@@ -166,6 +173,34 @@ extends NioStorageDriverBase<I, O> {
 				fileItem.size(), null, outBuffSize, null
 			);
 		} catch(final IOException e) {
+			createFileTask.setStatus(FAIL_IO);
+			throw new RuntimeException(e);
+		}
+	}
+
+	protected DFSInputStream getReadFileStream(
+		final DataIoTask<? extends DataItem> fileIoTask
+	) {
+		final String endpointAddr = fileIoTask.getNodeAddr();
+		final DFSClient endpoint = endpoints.get(endpointAddr);
+		final String srcPath = fileIoTask.getSrcPath();
+		if(srcPath == null || srcPath.isEmpty()) {
+			return null;
+		}
+		final DataItem fileItem = fileIoTask.getItem();
+		final String fileName = fileItem.getName();
+		final String filePath;
+		if(fileName.startsWith(srcPath)) {
+			filePath = fileName;
+		} else if(srcPath.endsWith("/")) {
+			filePath = srcPath + fileName;
+		} else {
+			filePath = srcPath + "/" + fileName;
+		}
+		try {
+			return endpoint.open(filePath, inBuffSize, false);
+		} catch(final IOException e) {
+			fileIoTask.setStatus(FAIL_IO);
 			throw new RuntimeException(e);
 		}
 	}
@@ -183,15 +218,25 @@ extends NioStorageDriverBase<I, O> {
 
 	private void invokeFileNio(final DataIoTask<? extends DataItem> fileIoTask) {
 		final IoType ioType = fileIoTask.getIoType();
+		DFSInputStream input = null;
+		DFSOutputStream output = null;
 		try {
 			switch(ioType) {
 				case NOOP:
 					invokeNoop((O) fileIoTask);
 					break;
 				case CREATE:
-					invokeFileCreate(fileIoTask);
+					input = fileInputStreams.computeIfAbsent(fileIoTask, this::getReadFileStream);
+					output = fileOutputStreams.computeIfAbsent(fileIoTask, this::getCreateFileStream);
+					if(input == null) {
+						invokeFileCreate(fileIoTask, output);
+					} else {
+						invokeFileCopy(fileIoTask, input, output);
+					}
 					break;
 				case READ:
+					input  = fileInputStreams.computeIfAbsent(fileIoTask, this::getReadFileStream);
+					invokeFileRead(fileIoTask, input);
 					break;
 				case UPDATE:
 					break;
@@ -218,7 +263,7 @@ extends NioStorageDriverBase<I, O> {
 				);
 				fileItem.size(countBytesDone);
 				finishIoTask((O) fileIoTask);
-				fileIoTask.setStatus(IoTask.Status.FAIL_IO);
+				fileIoTask.setStatus(FAIL_IO);
 			} else if(cause instanceof URISyntaxException) {
 				LogUtil.exception(Level.DEBUG, cause, "Failed to calculate the HDFS service URI");
 				fileItem.size(countBytesDone);
@@ -235,6 +280,28 @@ extends NioStorageDriverBase<I, O> {
 				finishIoTask((O) fileIoTask);
 				fileIoTask.setStatus(IoTask.Status.FAIL_UNKNOWN);
 			}
+		} finally {
+			
+			if(!ACTIVE.equals(fileIoTask.getStatus())) {
+
+				if(input != null) {
+					fileInputStreams.remove(fileIoTask);
+					try {
+						input.close();
+					} catch(final IOException e) {
+						Loggers.ERR.warn("Failed to close the source I/O channel");
+					}
+				}
+
+				if(output != null) {
+					fileOutputStreams.remove(fileIoTask);
+					try {
+						output.close();
+					} catch(final IOException e) {
+						Loggers.ERR.warn("Failed to close the destination I/O channel");
+					}
+				}
+			}
 		}
 	}
 
@@ -242,7 +309,9 @@ extends NioStorageDriverBase<I, O> {
 		finishIoTask(ioTask);
 	}
 
-	protected void invokeFileCreate(final DataIoTask<? extends DataItem> fileIoTask) {
+	protected void invokeFileCreate(
+		final DataIoTask<? extends DataItem> fileIoTask, final DFSOutputStream outputStream
+	) {
 		final DataItem fileItem = fileIoTask.getItem();
 		final long fileSize;
 		try {
@@ -251,26 +320,94 @@ extends NioStorageDriverBase<I, O> {
 			throw new AssertionError(e);
 		}
 		long countBytesDone = fileIoTask.getCountBytesDone();
-		final DFSOutputStream output = fileOutputStreams.computeIfAbsent(
-			fileIoTask, this::getCreateFileStream
-		);
 		try {
 			long remainingBytes = fileSize - countBytesDone;
 			if(remainingBytes > 0) {
-				countBytesDone += fileItem.writeToStream(output, remainingBytes);
+				final WritableByteChannel outputChan = IoUtil.getThreadLocalOutputChannel(
+					outputStream, remainingBytes
+				);
+				countBytesDone += fileItem.writeToSocketChannel(outputChan, remainingBytes);
 				fileIoTask.setCountBytesDone(countBytesDone);
 			} else {
 				finishIoTask((O) fileIoTask);
-				fileOutputStreams.remove(fileIoTask);
-				output.close();
 			}
 		} catch(final IOException e) {
 			LogUtil.exception(
 				Level.DEBUG, e, "Failed to write to the file: {}" + fileItem.getName()
 			);
+			finishIoTask((O) fileIoTask);
+			fileIoTask.setStatus(FAIL_IO);
+		}
+	}
+
+	protected void invokeFileCopy(
+		final DataIoTask<? extends DataItem> fileIoTask, final DFSInputStream inputStream,
+		final DFSOutputStream outputStream
+	) {
+		long countBytesDone = fileIoTask.getCountBytesDone();
+		final long fileSize;
+		try {
+			fileSize = fileIoTask.getItem().size();
+		} catch(final IOException e) {
+			throw new AssertionError(e);
+		}
+		final long remainingSize = fileSize - countBytesDone;
+		if(remainingSize > 0 && ACTIVE.equals(fileIoTask.getStatus())) {
+			final byte[] buff = new byte[
+				remainingSize > REUSABLE_BUFF_SIZE_MAX ?
+					REUSABLE_BUFF_SIZE_MAX : (int) remainingSize
+			];
+			try {
+				final int n = inputStream.read(buff, 0, buff.length);
+				outputStream.write(buff, 0, n);
+				countBytesDone += n;
+				fileIoTask.setCountBytesDone(countBytesDone);
+			} catch(final IOException e) {
+				LogUtil.exception(
+					Level.DEBUG, e, "Failed to copy the file: {}" + fileIoTask.getItem().getName()
+				);
+				finishIoTask((O) fileIoTask);
+				fileIoTask.setStatus(FAIL_IO);
+			}
+		}
+		if(countBytesDone == fileSize) {
+			finishIoTask((O) fileIoTask);
+		}
+	}
+
+	protected void invokeFileRead(
+		final DataIoTask<? extends DataItem> fileIoTask, final DFSInputStream inputStream
+	) {
+		final DataItem fileItem = fileIoTask.getItem();
+		final long fileSize;
+		try {
+			fileSize = fileItem.size();
+		} catch(final IOException e) {
+			throw new AssertionError(e);
+		}
+		long countBytesDone = fileIoTask.getCountBytesDone();
+		final DFSInputStream input = fileInputStreams.computeIfAbsent(
+			fileIoTask, this::getReadFileStream
+		);
+		try {
+			long remainingBytes = fileSize - countBytesDone;
+			if(remainingBytes > 0) {
+				if(verifyFlag) {
+					countBytesDone += fileItem.readAndVerify(input, remainingBytes);
+					fileIoTask.setCountBytesDone(countBytesDone);
+				} else {
+					input.read
+				}
+			} else {
+				finishIoTask((O) fileIoTask);
+				fileOutputStreams.remove(fileIoTask);
+				input.close();
+			}
+		} catch(final IOException e) {
+			LogUtil.exception(Level.DEBUG, e, "Failed to read the file: {}" + fileItem.getName());
 			fileItem.size(countBytesDone);
 			finishIoTask((O) fileIoTask);
-			fileIoTask.setStatus(IoTask.Status.FAIL_IO);
+			fileIoTask.setStatus(FAIL_IO);
 		}
 	}
 
@@ -307,7 +444,7 @@ extends NioStorageDriverBase<I, O> {
 			);
 			fileIoTask.startResponse();
 			fileIoTask.finishResponse();
-			fileIoTask.setStatus(IoTask.Status.FAIL_IO);
+			fileIoTask.setStatus(FAIL_IO);
 		}
 	}
 
