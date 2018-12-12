6ee561646d8be6ef8502b3992f601b37c1ce35aa
Andrey Kurilov <andrey.kurilov@emc.com>
2018-12-12 13:29:01 +0300

    v0.2.2 try gl ci

--- src/test/java/com/emc/mongoose/storage/driver/hdfs/util/docker/DockerHost.java
+++ src/test/java/com/emc/mongoose/storage/driver/hdfs/util/docker/DockerHost.java
@@ -1,8 +1,24 @@
 package com.emc.mongoose.storage.driver.hdfs.util.docker;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public interface DockerHost {
 
-	String ENV_NAME = "DOCKER_HOST";
+	Pattern ENV_VALUE_PATTERN = Pattern.compile("[\\w\\d]+://([\\w\\d\\\\.\\-_]+):\\d{1,5}");
 
-	String ENV_SVC_HOST = System.getenv(ENV_NAME) == null ? "127.0.0.1" : System.getenv(ENV_NAME);
+	String ENV_SVC_HOST = envDockerHost() == null ? "127.0.0.1" : extractHost(envDockerHost());
+
+	static String envDockerHost() {
+		return System.getenv("DOCKER_HOST");
+	}
+
+	static String extractHost(final String envDockerHost) {
+		final Matcher m = ENV_VALUE_PATTERN.matcher(envDockerHost);
+		if(m.find()) {
+			return m.group(1);
+		} else {
+			throw new AssertionError("Invalid docker host env value: " + envDockerHost);
+		}
+	}
 }
