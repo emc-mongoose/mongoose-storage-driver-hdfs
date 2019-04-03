49d2bf56e9b429576203d8eb6a6cb7cac6a809a4
Andrey Kurilov <andrey.kurilov@emc.com>
2019-04-03 12:59:51 +0300

    try gl services

--- src/test/java/com/emc/mongoose/storage/driver/hdfs/util/docker/DockerHost.java
+++ src/test/java/com/emc/mongoose/storage/driver/hdfs/util/docker/DockerHost.java
@@ -1,15 +1,10 @@
 package com.emc.mongoose.storage.driver.hdfs.util.docker;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public interface DockerHost {
 
-	Pattern ENV_VALUE_PATTERN = Pattern.compile("[\\w\\d]+://([\\w\\d\\\\.\\-_]+):\\d{1,5}");
+	String ENV_SVC_HOST = isGitLabCiEnv() ? "storage" : "127.0.0.1";
 
-	String ENV_SVC_HOST = envDockerHost() == null ? "127.0.0.1" : envDockerHost();
-
-	static String envDockerHost() {
-		return System.getenv("SERVICE_HOST");
+	static boolean isGitLabCiEnv() {
+		return null == System.getenv("CI");
 	}
 }
