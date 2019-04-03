cc7501b7fa96a66b78ab9d55b7b2f2cfe121e49c
Andrey Kurilov <andrey.kurilov@emc.com>
2019-04-03 13:02:51 +0300

    try gl services

--- src/test/java/com/emc/mongoose/storage/driver/hdfs/util/docker/DockerHost.java
+++ src/test/java/com/emc/mongoose/storage/driver/hdfs/util/docker/DockerHost.java
@@ -5,6 +5,6 @@ public interface DockerHost {
 	String ENV_SVC_HOST = isGitLabCiEnv() ? "storage" : "127.0.0.1";
 
 	static boolean isGitLabCiEnv() {
-		return null == System.getenv("CI");
+		return null != System.getenv("CI");
 	}
 }
